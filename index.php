30b1f81e56fd60b6bb6ec086609c4bf27f4ec153
Mohaimenul Adnan <adnan007.id@gmail.com>
2015-04-21 18:01:30 +0000

    Updated version notation

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: Business Reviews
 Plugin URI: http://knockoutwebsolutions.com/business-reviews
 Description: Let visitors of your website review your business and show off the reviews at the same time.
-Version: 1.3
+Version: 1.4
 Author: Mohaiemnul Adnan, KnockoutWebSolutions
 Author URI: http://knockoutwebsolutions.com
 License: Personal
